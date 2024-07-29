fc576f64d8aefa1f8be5a15d5bbd1e55c45a02b0
Bertrand Yann <yann.bertrand@ibot.cas.cz>
2024-07-29 10:19:31 +0000

    Replace gfa_graph.py

--- nextpiper/src/gfa_graph.py
+++ nextpiper/src/gfa_graph.py
@@ -17,28 +17,36 @@ __version__ = "0.1"
 # =======================================================================================
 #               IMPORTS
 # =======================================================================================
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from dataclasses import dataclass, field
+from operator import itemgetter, attrgetter
+from itertools import chain, groupby
 from pathlib import Path
-from typing import Self, Literal
+from typing import Self, NewType, List, Dict, Tuple, Optional, Literal
 import sys
 
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
-from graph_alns_parser import Read, oriented_edge
-from gfa2fasta import SeqPath
+from graph_alns_parser import Read
+
+oriented_edge = NewType("oriented_edge", tuple[str, Literal["+", "-"]])
 
 
 # =============================================================================
 #                CLASSES
 # =============================================================================
-
-LinkSupport = dict[tuple[str], int]
-
-
 @dataclass(frozen=True)
 class Path_on_graph:
+    """
+        Encodes the path on the graph matching either a sequence (SPAligner) or a hmm profile (PathRacer).
+    Attributes
+    ----------
+    -start: coordinate of the starting position on the first edge.
+    -end: coordinate of the ending position on the last edge.
+    -edges: list of 'oriented_edge' tuples.
+    """
+
     start: int
     end: int
     edges: list[oriented_edge]
@@ -49,6 +57,21 @@ class Path_on_graph:
 
 @dataclass
 class Edge:
+    """
+        Encodes the edge of the graph, i.e. the 'S' line in a gfa file.
+        Keeps information about matching elements, that is sequences from SPAligner or hmm profile from PathRacer.
+    Attributes
+    ----------
+    -id: edge number.
+    -raw_seq: nucleotide sequence including k-mer overlap.
+    -coverage: k-mer coverage.
+
+    Post init
+    ----------
+    -seq: 'raw_seq' converted to Bio.Seq.Seq object.
+    -matching_exons: dictionary of matches (hmm or sequences, needs to be worked out).
+    """
+
     id: str
     raw_seq: str
     coverage: float
@@ -78,13 +101,27 @@ class Edge:
 
 @dataclass
 class Assembly_graph:
+    """
+        Encodes the gfa assembly graph. Provides methods for graph traversal and sequence retrieval.
+    Attributes
+    ----------
+    -gfa_filename: complete path to gfa assembly file.
+    Post init
+    -K: k-mer used during SPAdes assembly (edge overlap value).
+    -edge_dict: edge number mapping to their respective Edge objects. Used for sequence retrieval.
+    -graph: encoding of the graph structure, with an edge mapping.
+        Each node represented symmetrically as (1,+)->(2,+) and (2,-)->(1,-).
+    -linked_edges: information about non-adjacent edges, that are connected through mate-reads.
+    ----------
+    """
+
     gfa_filename: str
     K: int = field(init=False)
     edge_dict: dict[str, Edge] = field(default_factory=dict, init=False)
     graph: dict[oriented_edge, list[oriented_edge]] = field(
         default_factory=lambda: defaultdict(list), init=False
     )
-    linked_edges: LinkSupport = field(
+    linked_edges: dict[tuple[str], int] = field(
         default_factory=lambda: defaultdict(int), init=False
     )
     rev = {"+": "-", "-": "+"}
@@ -99,18 +136,18 @@ class Assembly_graph:
             for line in file:
                 match line[0]:
                     case "H":
-                        header = line
+                        pass
                     case "S":
                         edge_line = line.strip().split("\t")[1:]
-                        node_id, seq, kc = edge_line[0], edge_line[1], edge_line[-1]
-                        coverage = float(kc[len("KC:i:") :]) / len(seq)
+                        node_id, seq, _kc = edge_line[0], edge_line[1], edge_line[-1]
+                        coverage = float(_kc[len("KC:i:") :]) / len(seq)
                         self.edge_dict[node_id] = Edge(node_id, seq, coverage)
 
                     case "L" | "J":
-                        _, node_id1, pos1, node_id2, pos2, match = line.strip().split(
+                        _, node_id1, pos1, node_id2, pos2, _match = line.strip().split(
                             "\t"
                         )
-                        self.K = int(match[:-1])
+                        self.K = int(_match[:-1])
                         self.graph[(node_id1, pos1)].append((node_id2, pos2))
                         self.graph[(node_id2, self.rev[pos2])].append(
                             (node_id1, self.rev[pos1])
@@ -120,34 +157,8 @@ class Assembly_graph:
                             f"ERROR: found line of type {line[0]}"
                         )
 
-    def link_edges(self, reads: list[Read]) -> Self:
-        """Given a list of paired reads, compute the link support that those pairs
-        exhibit. Each pair that connects a composition of edges is added as a
-        supported link, increasing its count in the dictionary. Compositions of
-        a single edge are not taken into account.
-        """
-
-        get_edges = lambda frags: (frag.edge.edge_id for frag in frags)
-
-        for read in reads:
-            edges = get_edges(read.fragments + read.mate.fragments)
-            if len(unique_edges := set(edges)) > 1:
-                self.linked_edges[tuple(sorted(unique_edges))] += 1
-
+    def link_edges(self, links: list[Read]) -> Self:
         return self
-
-    def path_support(self, path: SeqPath) -> LinkSupport:
-        "Return the links support that are congruent with the given path."
-
-        if not self.linked_edges:
-            raise ValueError("No link information in the graph to evaluate path.")
-
-        path_edges = {edge[:-1] for edge in path.path}
-        return {
-            link: support
-            for link, support in self.linked_edges.items()
-            if path_edges.issuperset(set(link))
-        }
 
     def _retrieve_path(
         self,
@@ -190,7 +201,7 @@ class Assembly_graph:
 
     def retrieve_path(self, name: str, edge_paths: Path_on_graph) -> SeqRecord:
         """
-        Retrieve the sequence corresponding to a graph transversoal.
+        Retrieve the sequence corresponding to a graph transversal.
         :param name: Name of the sequence, should correspond to the name of the consensus sequence or hmm profile.
         :param edge_paths: Path_on_graph object
         :return:
