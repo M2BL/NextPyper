fb5d24bedc019e101fe6f45c22644aff6aebf97f
Corrales Simon <simon.corrales@ibot.cas.cz>
2025-07-24 12:25:14 +0200

    Partial (broken) kmer params refactoring

--- nextpyper/src/seeds_collection.py
+++ nextpyper/src/seeds_collection.py
@@ -20,11 +20,13 @@ __version__ = "0.1"
 
 from pathlib import Path
 from collections import Counter, defaultdict
+import json
 import re
 
 from Bio import SeqIO
 import numpy as np
 import polars as pl
+from more_itertools import one
 
 from multi_seq_probes import group_probes
 
@@ -51,14 +53,62 @@ REC_PAT = r"^(.*?)-(.*?)_.*_cov_([\d\.]+)$"
 # =============================================================================
 
 
+def saute_kmer_params(wildcards, input):
+    # Read the median kmer cov in spades scaffolds
+    med_cov = float(Path(input.cov).read_text())
+    # Read the read lenght of reads
+    with open(input.json) as file:
+        summary = json.load(file)
+        L = int(summary["summary"]["after_filtering"]["read2_mean_length"])
+
+    # Read the Kmer size used by spades
+    spades_folder = outdir / f"assembled/spades/{wildcards.sample}"
+    kspades = int(one(spades_folder.glob("K*/final_contigs.paths")).parent.name[1:])
+
+    # Compute median depth observed
+    read_med_cov = med_cov * L / (L - kspades + 1)
+
+    # Compute kmer sizes for the given targets
+    k2nd = int(L * (1 - (secondary_target_depth / read_med_cov)) + 1)
+    k1st = int(L * (1 - (primary_target_depth / read_med_cov)) + 1)
+
+    # Adjust kmer to ranges kmer: 1st [0.5 - 0.8]L, 2nd [0.15 - 0.3]L
+    k2nd_min, k2nd_max = int(L * 0.15), int(L * 0.3)
+    k1st_min, k1st_max = int(L * 0.5), int(L * 0.8)
+
+    if k2nd < k2nd_min:
+        k2nd = k2nd_min
+    elif k2nd > k2nd_max:
+        k2nd = k2nd_max
+
+    if k1st < k1st_min:
+        k1st = k1st_min
+    elif k1st > k1st_max:
+        k1st = k1st_max
+
+    # Cap secondary kmer on 21 and ensure odd kmers
+    k2nd = 21 if k2nd < 21 else k2nd
+    k2nd = k2nd + 1 if k2nd % 2 == 0 else k2nd
+    k1st = k1st + 1 if k1st % 2 == 0 else k1st
+
+    return f"--kmer {k1st} --secondary_kmer {k2nd}"
+
+
 def snakemake_call(snakemake):
 
     cluster_tables_dir = snakemake.input.cluster_tables
     sample_probes_dir = snakemake.input.samples
+    spades_folders = snakemake.input.spades_folders
     probes_path = snakemake.input.probes
+
     seeds_out = snakemake.output.seeds
+    saute_params = snakemake.output.saute_params
+
     probes_pattern = snakemake.params.pattern
     multi_probes = snakemake.params.is_multi
+    primary_target_depth = snakemake.params.primary_target_depth
+    secondary_target_depth = snakemake.params.secondary_target_depth
+
     min_sister_freq = snakemake.params.min_sister_freq
     cov_log_dir = Path(snakemake.log[0]).parent
 
@@ -70,6 +120,13 @@ def snakemake_call(snakemake):
         }
         for sample in map(Path, sample_probes_dir)
     }
+
+    pat = re.compile(REC_PAT)
+
+    for sample, probe_recs in sample_recs.items():
+        med_cov = np.median(
+            [float(pat.search(rec)[3]) for recs in probe_recs.values() for rec in recs]
+        )
 
     # Read the probes in order to supplement the seeds in case they are missing
     probes = list(SeqIO.parse(probes_path, "fasta"))
@@ -85,7 +142,6 @@ def snakemake_call(snakemake):
             rec.id = f"{rec.id}-{probe}_EDGE_0_length_{len(rec)}_cov_0.0"
             rec.name = rec.description = ""
 
-    pat = re.compile(REC_PAT)
     probe_tables = {}
     sister_samples = defaultdict(Counter)
     for probe in map(Path, cluster_tables_dir):
